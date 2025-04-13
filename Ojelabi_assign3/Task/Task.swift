cb38bfd1d6fa4140e9922456310f5c0f5d279391
ojel0001 <ojel0001@algonquinlive.com>
2025-04-13 17:14:43 -0400

    create the profile and task for the task manager app

---
+++ Ojelabi_assign3/Task/Task.swift
@@ -0,0 +1,18 @@
+//
+//  Task.swift
+//  Ojelabi_assign3
+//
+//  Created by sunny ojelabi on 2025-04-13.
+//
+
+import SwiftUI
+
+struct Task: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Task()
+}
