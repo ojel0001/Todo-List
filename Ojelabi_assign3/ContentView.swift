19e03199bc44606704de72d3f7713fb39d0bb84b
ojel0001 <ojel0001@algonquinlive.com>
2025-04-13 15:24:29 -0400

    Initial Commit

---
+++ Ojelabi_assign3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ojelabi_assign3
+//
+//  Created by sunny ojelabi on 2025-04-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ojelabi_assign3/Ojelabi_assign3App.swift
@@ -0,0 +1,17 @@
+//
+//  Ojelabi_assign3App.swift
+//  Ojelabi_assign3
+//
+//  Created by sunny ojelabi on 2025-04-13.
+//
+
+import SwiftUI
+
+@main
+struct Ojelabi_assign3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
