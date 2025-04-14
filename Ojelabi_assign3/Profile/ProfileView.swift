5fa23ee3cfe691a44a8da861445f60f0d1626793
ojel0001 <ojel0001@algonquinlive.com>
2025-04-13 20:40:45 -0400

    create the profile view

---
+++ Ojelabi_assign3/Profile/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  Ojelabi_assign3
+//
+//  Created by sunny ojelabi on 2025-04-13.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
---
+++ Ojelabi_assign3/Task/TaskListView.swift
@@ -0,0 +1,18 @@
+//
+//  TaskListView.swift
+//  Ojelabi_assign3
+//
+//  Created by sunny ojelabi on 2025-04-13.
+//
+
+import SwiftUI
+
+struct TaskListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskListView()
+}
