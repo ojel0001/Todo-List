3dfd64517a48e3d8d60ceca08d5039a24fedc1cc
ojel0001 <ojel0001@algonquinlive.com>
2025-04-13 21:26:44 -0400

    create taskListView and MaintabView and complete mainTab View and alllowed it to tab between the tasks listed and the profle of the user

---
+++ Ojelabi_assign3/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  Ojelabi_assign3
+//
+//  Created by sunny ojelabi on 2025-04-13.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
