97e6644ac2e9d8e22f2f07bf7782d9189fb0ee9e
ojel0001 <ojel0001@algonquinlive.com>
2025-04-16 01:56:17 -0400

    finshed the addTaskView file which always the user to add task from the list view

---
+++ Ojelabi_assign3/Task/AddTaskView.swift
@@ -0,0 +1,18 @@
+//
+//  AddTaskView.swift
+//  Ojelabi_assign3
+//
+//  Created by sunny ojelabi on 2025-04-14.
+//
+
+import SwiftUI
+
+struct AddTaskView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddTaskView()
+}
---
+++ Ojelabi_assign3/Task/TaskDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  TaskDetailView.swift
+//  Ojelabi_assign3
+//
+//  Created by sunny ojelabi on 2025-04-14.
+//
+
+import SwiftUI
+
+struct TaskDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskDetailView()
+}
