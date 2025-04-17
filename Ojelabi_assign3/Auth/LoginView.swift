9c42aad26f2df0948a635132127f0e7ea1da901e
ojel0001 <ojel0001@algonquinlive.com>
2025-04-17 02:48:12 -0400

    completed the content view screen and started the login auth screen

---
+++ Ojelabi_assign3/Auth/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  Ojelabi_assign3
+//
+//  Created by sunny ojelabi on 2025-04-17.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
