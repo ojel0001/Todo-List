9ec500da6d44b481f4e5adf9c41353338e512bc9
ojel0001 <ojel0001@algonquinlive.com>
2025-04-18 19:56:34 -0400

    finish all the firebase code and the application is fully functional

---
+++ Ojelabi_assign3/Task/FirestoreTaskService..swift
@@ -0,0 +1,18 @@
+//
+//  FirestoreTaskService..swift
+//  Ojelabi_assign3
+//
+//  Created by sunny ojelabi on 2025-04-18.
+//
+
+import SwiftUI
+
+struct FirestoreTaskService_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FirestoreTaskService_()
+}
